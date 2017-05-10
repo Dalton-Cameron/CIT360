e593fa716f41ae5db5babd7209d01bf9abaa1e26
Cameron Dalton <dal14004@byui.edu>
2017-05-09 20:40:17 -0600

    This is a commit to show my thread program

--- CIT360/src/test/MyClass.java
+++ CIT360/src/test/MyClass.java
@@ -1,5 +1,23 @@
 package test;
 
-public class MyClass {
+public class MyClass extends Thread{
+	public void run(){
+		for(int i=0; i<3;i++)
+			System.
+			out.println("Thread id:" + Thread.currentThread().getName());
+		try{
+			Thread.currentThread().sleep(100);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
 
+public static void main(String[] args){
+	for (int threadCnt=0; threadCnt<3;threadCnt++){
+		MyClass aMyClass = new MyClass();
+		aMyClass.start();
+	}
+	
+}
 }
