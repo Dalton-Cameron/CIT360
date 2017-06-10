a3abd9f57f5cb2a8fcfeba384d37b10415e64e00
Cameron Dalton <dal14004@byui.edu>
2017-06-10 13:57:52 -0600

    This code shows how you can implement the runnable interface to use runnables in your code.

---
+++ CIT360/src/test/Runnables.java
@@ -0,0 +1,39 @@
+package test;
+
+class MyRunnableThread implements Runnable{
+	 
+    public static int myCount = 0;
+    public MyRunnableThread(){
+         
+    }
+    public void run() {
+        while(MyRunnableThread.myCount <= 20){
+            try{
+                System.out.println("Example Thread: "+(++MyRunnableThread.myCount));
+                Thread.sleep(100);
+            } catch (InterruptedException excp1) {
+                System.out.println("Exception in thread: "+excp1.getMessage());
+            }
+        }
+    }
+}
+
+
+public class Runnables {
+    public static void main(String a[]){
+        System.out.println("This Starts the Main Thread>");
+        MyRunnableThread runnablethread = new MyRunnableThread();
+        Thread t = new Thread(runnablethread);
+        t.start();
+        while(MyRunnableThread.myCount <= 10){
+            try{
+                System.out.println("Main Thread: "+(++MyRunnableThread.myCount));
+                Thread.sleep(100);
+            } catch (InterruptedException excp2){
+                System.out.println("Exception in main thread: "+excp2.getMessage());
+            }
+        }
+        System.out.println("This is the End of the Main Thread>");
+    }
+}
+
